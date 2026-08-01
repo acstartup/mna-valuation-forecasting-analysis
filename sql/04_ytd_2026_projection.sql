0568350e981b5e6151e3e61e7fa37d4fa0a5567e
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-08-01 16:51:51 -0400

    (feat): 2026 projection data, non-client data, awaiting client results, does include processed csv table

---
+++ sql/04_ytd_2026_projection.sql
@@ -0,0 +1,20 @@
+-- ====================================================================
+-- Project: M&A Valuation & Financial Forecast Analysis
+-- Script: 04_ytd_2026_projection.sql
+-- Purpose: Create projections for end of 2026 revenue, expense,
+--          and profit based on Jan-Aug data.
+-- Created By: Aiden Chen
+-- ====================================================================
+
+-- Step 1: Backup company_financials Into Processed Version
+CREATE TABLE company_financials_incl_2026 AS
+SELECT *
+FROM company_financials
+
+-- Step 2: Calculations For (YTD/Months-into-2026) * 12, Data NOT From Client (Awaiting Client Data)
+INSERT INTO company_financials(year, gross_income, expense, net_profit)
+VALUES (2026, (4813322.50/8) * 12, (4489517.21/8) * 12, 485707.93)
+
+-- Step 3: Review Processed Table Output
+SELECT *
+FROM company_financials_incl_2026
