6b3739ca9fb95426e4b3751e9727778a313dc340
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-08-01 14:13:22 -0400

    (fix): seperated creating schema and data ingestion and profitability kpis

---
+++ sql/01_schema_and_ingestion.sql
@@ -0,0 +1,20 @@
+-- ====================================================================
+-- Project: M&A Valuation & Financial Forecasting Analysis
+-- Script: 01_schema_and_ingestion.sql
+-- Purpose: Create company_financials table schema and insert 
+--          client-given downloaded csv data for M&A into table.
+-- Created By: Aiden Chen
+-- ====================================================================
+
+-- Step 1: Database Schema Creation
+CREATE TABLE IF NOT EXISTS company_financials (
+    year INT PRIMARY KEY,
+    gross_income NUMERIC(12, 2) NOT NULL,
+    expense NUMERIC(12, 2) NOT NULL,
+    net_profit NUMERIC(12, 2) NOT NULL
+);
+
+-- Step 2: Data Insertion From Files (Client-Given)
+COPY company_financials(year, gross_income, expense, net_profit)
+FROM '/tmp/mna_data.csv'
+WITH (FORMAT csv, HEADER true);
---
+++ sql/02_profitability_kpis.sql
@@ -0,0 +1,23 @@
+-- ====================================================================
+-- Project: M&A Valuation & Financial Forecasting Analysis
+-- Script: 01_profitability_margin_analysis.sql
+-- Purpose: Calculate profitability margin metrics: net profit 
+--          margin %, expense to revenue %
+-- Created By: Aiden Chen
+-- ====================================================================
+
+-- Step 1: Create Profibility Margins View With Net Margin % & Expense Ratio %
+CREATE OR REPLACE VIEW v_profitability_kpis AS
+SELECT
+    year,
+    gross_income,
+    expense,
+    net_profit,
+    ROUND((net_profit/gross_income) * 100, 2) AS net_margin_pct,
+    ROUND((expense/gross_income) * 100, 2) AS expense_revenue_pct
+FROM company_financials;
+
+-- Step 2: Report Output Query
+SELECT * 
+FROM v_profitability_kpis
+ORDER BY year ASC;
