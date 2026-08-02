b83ad50456fe66d045b6509bbadc61bf399d393d
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-08-02 09:44:51 -0400

    (fix): 03 net_profit to profit

--- sql/03_growth_kpis.sql
+++ sql/03_growth_kpis.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE VIEW v_growth_kpis AS
             year,
             revenue,
             expense,
-            net_profit,
+            profit,
             LAG(revenue, 1) OVER (ORDER BY year) AS prior_year_revenue,
             LAG(revenue, 10) OVER (ORDER BY year ASC) AS ten_year_prior_revenue
             LAG(expense, 10) OVER (ORDER BY year ASC) AS ten_year_prior_expense
@@ -22,7 +22,7 @@ CREATE OR REPLACE VIEW v_growth_kpis AS
         c.year,
         c.revenue,
         c.expense,
-        c.net_profit,
+        c.profit,
         ROUND((c.revenue - p.prior_year_revenue)/(p.prior_year_revenue) * 100, 2) AS yoy_growth_pct,
         ROUND(POWER((c.revenue/p.ten_year_prior_revenue) * 100, (1.0/10.0)) - 1, 2) AS ten_year_cagr,
         ROUND(POWER((c.expense/p.ten_year_prior_expense) * 100, (1.0/10.0)) - 1, 2) AS ten_year_expense_cagr
